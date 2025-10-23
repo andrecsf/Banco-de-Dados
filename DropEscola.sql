efd323717dc9eb508b3d071f0d6b1156e8fd2d8c
andrecsf <123967995+andrecsf@users.noreply.github.com>
2025-10-23 19:16:22 -0300

    Update DropEscola.sql

--- DropEscola.sql
+++ DropEscola.sql
@@ -1,7 +1,6 @@
 -- Drop Database
 
 use escolamusica;
-start transaction;
 drop table if exists musico_funcao_sinfonia;
 drop table if exists musico_instrumento;
 drop table if exists musico;
@@ -9,6 +8,15 @@ drop table if exists sinfonia;
 drop table if exists funcao;
 drop table if exists instrumento;
 drop table if exists orquestra;
-rollback;
+drop view if exists musicos_info_orquestra_info;
+drop view if exists sinfonia_detalhada;
+drop view if exists instrumento_musico;
+drop view if exists participacao_sinfonia;
+drop view if exists estatisticas_orquestra;
+drop view if exists musicos_faixa_etaria;
+drop view if exists funcao_instrumento;
+drop view if exists composicoes_autor;
+drop view if exists musicos_multinstrumentalistas;
+drop view if exists instrumentos_populares;
 
 drop database escolamusica;
